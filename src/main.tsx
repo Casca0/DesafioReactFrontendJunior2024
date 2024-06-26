39a3b0584ed8ee93010d1aedb29222bece461936
Casca0 <lucashborzani@gmail.com>
2024-06-26 13:07:51 -0300

    Resolved build issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import TodoTaskProvider from './provider.tsx';
+import App from './App';
+import TodoTaskProvider from './provider';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 
@@ -19,4 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		</TodoTaskProvider>
 	</React.StrictMode>
 );
+
+
 
